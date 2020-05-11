f1013cff64535bd9c9ad8f3066e08a8800d36d3b
Help-14 <help-14@hotmail.com>
2020-05-11 11:01:25 +0700

    temp fix insert to db

--- routes/api/log.js
+++ routes/api/log.js
@@ -101,26 +101,18 @@ router.post("/:serverid", async function (req, res, next) {
 
 	// Insert to Database
 	try {
-		await db.query(`INSERT INTO logs(
-            cpu, 
-            memory, 
-            network, 
-            disk, 
-            os, 
-            process, 
-            file, 
-            time,
-            server) 
-        VALUES (
-            '${data.cpu ? JSON.stringify(data.cpu) : "{}"}',
-            '${data.memory ? JSON.stringify(data.memory) : "{}"}',
-            '${data.network ? JSON.stringify(data.network) : "{}"}',
-            '${data.disk ? JSON.stringify(data.disk) : "{}"}',
-            '${data.os ? JSON.stringify(data.os) : "{}"}',
-            '${data.process ? JSON.stringify(data.process) : "{}"}',
-            '${data.file ? JSON.stringify(data.file) : "{}"}',
-            ${data.time},
-            '${server}')`);
+		await db.query(`INSERT INTO logs(cpu, memory, network, disk, os, process, file, time, server) 
+        				VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::text, $7::text, $8, $9::text')`,
+			[
+				data.cpu ? JSON.stringify(data.cpu) : "{}",
+				data.memory ? JSON.stringify(data.memory) : "{}",
+				data.network ? JSON.stringify(data.network) : "{}",
+				data.disk ? JSON.stringify(data.disk) : "{}",
+				data.os ? JSON.stringify(data.os) : "{}",
+				data.process ? JSON.stringify(data.process) : "{}",
+				data.file ? JSON.stringify(data.file) : "{}",
+				data.time, server
+			]);
 
 		res.json({ result: true });
 	} catch (err) {
